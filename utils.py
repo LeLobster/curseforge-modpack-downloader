429ddb84b8cff53a1988e484f33988818b1713f1
lelobster <lelobster@protonmail.com>
2020-06-07 17:16:18 +0200

    added param strict to is_valid_path func when True will not allow path to already exist

--- utils.py
+++ utils.py
@@ -18,19 +18,23 @@ def get_full_path(path: str) -> pathlib.Path:
     return path
 
 
-def is_valid_path(path: str) -> bool:
+def is_valid_path(path: str, strict=False) -> bool:
     """
     Checks if a path is valid, ie
      if a file or directory exists, and
      if it is readable/writable
 
     :param path:    The path
+    :param strict:  Don't allow path to already exist
     :return:        If the path is valid
     """
     path = pathlib.Path(path)
-    if path.exists():
+    if path.exists() and not strict:
         if path.is_file():
             return os.access(path, os.R_OK)  # read
-        if path.is_dir():
+        elif path.is_dir():
             return os.access(path, os.W_OK)  # write
-    return False
+    elif not path.exists() and strict:
+        return True
+    else:
+        return False
